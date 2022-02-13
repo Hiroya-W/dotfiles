a873794fce35c28b4101927b0340714f31449b63
Hiroya-W <hyuyukun@gmail.com>
2022-02-14 03:22:07 +0900

    :+1: Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,42 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+function fish_greeting
+    echo ""
 end
+
+# cd + ls にする
+function cd
+    if test (count $argv) -eq 0
+        builtin cd
+    else if test (count $argv) -gt 1
+        printf "%s\n" (_ "Too many args for cd command")
+        return 1
+    end
+    # Avoid set completions.
+    set -l previous $PWD
+
+    if test "$argv" = "-"
+        if test "$__fish_cd_direction" = "next"
+            nextd
+        else
+            prevd
+        end
+        return $status
+    end
+    builtin cd $argv
+    set -l cd_status $status
+    # Log history
+    if test $cd_status -eq 0 -a "$PWD" != "$previous"
+        set -q dirprev[$MAX_DIR_HIST]
+        and set -e dirprev[1]
+        set -g dirprev $dirprev $previous
+        set -e dirnext
+        set -g __fish_cd_direction prev
+    end
+
+    if test $cd_status -ne 0
+        return 1
+    end
+    ls
+    return $status
+end
+
+eval (starship init fish)
