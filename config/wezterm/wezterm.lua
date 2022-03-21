c12a8317d2b823296b5bc05ee890dbfa3068e97a
Hiroya-W <hyuyukun@gmail.com>
2022-03-21 16:27:24 +0900

    :sparkles: Add wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm';
+
+return {
+    font = wezterm.font("HackGenNerd Console"),
+    use_ime = true,
+    font_size = 11.0;
+    color_scheme = "MaterialDesignColors",
+    hide_tab_bar_if_only_one_tab = true,
+    adjust_window_size_when_changing_font_size = false,
+}
