fd2b5d88d3ffbbe49be7db2c0626c686d4a9c610
Hiroya-W <hyuyukun@gmail.com>
2022-05-02 00:36:08 +0900

    :wrench: Add colorscheme plugin

--- config/nvim/lua/rc/plugins.lua
+++ config/nvim/lua/rc/plugins.lua
@@ -1,4 +1,22 @@
-vim.cmd("packadd packer.nvim")
+vim.cmd[[packadd packer.nvim]]
 
 require("packer").startup(function()
+    -- Library
+    use "nvim-lua/popup.nvim"
+    use "nvim-lua/plenary.nvim"
+    use "kyazdani42/nvim-web-devicons"
+    use {
+        "rcarriga/nvim-notify", 
+        event = "VimEnter"
+    }
+
+    -- Colorscheme
+    use {
+        'folke/tokyonight.nvim',
+        config = function()
+            vim.g.tokyonight_style = "night"
+            vim.cmd[[colorscheme tokyonight]]
+        end,
+    }
 end)
+
