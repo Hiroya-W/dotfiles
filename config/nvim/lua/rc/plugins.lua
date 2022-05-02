b5bae0965b574db63e19905c7e2e56d61aceeaf7
Hiroya-W <hyuyukun@gmail.com>
2022-05-02 21:38:32 +0900

    :sparkles: Automatically install and setup packer.nvim

--- config/nvim/lua/rc/plugins.lua
+++ config/nvim/lua/rc/plugins.lua
@@ -1,3 +1,9 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 vim.cmd[[packadd packer.nvim]]
 
 require("packer").startup(function()
@@ -18,5 +24,11 @@ require("packer").startup(function()
             vim.cmd[[colorscheme tokyonight]]
         end,
     }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
 
