b83d543ee8db143d0fe56849a920a6a321102b51
Hiroya-W <hyuyukun@gmail.com>
2022-05-03 18:06:10 +0900

    :sparkles: Add configs for lsp

--- config/nvim/lua/rc/cmp.lua
+++ config/nvim/lua/rc/cmp.lua
@@ -99,10 +99,3 @@ cmp.setup.cmdline(':', {
         { name = 'cmdline' }
     })
 })
-
--- Setup lspconfig.
---local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
---require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
---    capabilities = capabilities
---}
--- config/nvim/lua/rc/lsp/configs.lua
+++ config/nvim/lua/rc/lsp/configs.lua
@@ -7,10 +7,25 @@ end
 -- https://github.com/neovim/nvim-lspconfig/wiki/Installing-language-servers#automatically
 local servers = {"sumneko_lua", "rust_analyzer"}
 
-require("nvim-lsp-installer").setup ({
-    ensure_installed = servers,
+require("nvim-lsp-installer").setup({
+    ensure_installed = servers, -- ensure these servers are always installed
+    automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+    ui = {
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
 })
 
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, server in ipairs(servers) do
-    require('lspconfig')[server].setup {}
+    local opts = {
+        capabilities = capabilities
+    }
+    require('lspconfig')[server].setup {
+        opts
+    }
 end
