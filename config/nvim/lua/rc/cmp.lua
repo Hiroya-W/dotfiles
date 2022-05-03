90e7f4b1a03cd77f2e0a41809cab6d339603d4f3
Hiroya-W <hyuyukun@gmail.com>
2022-05-03 13:45:32 +0900

    :recycle: Move nvim-cmp.lua to cmp.lua

--- config/nvim/lua/rc/cmp.lua
+++ config/nvim/lua/rc/cmp.lua
@@ -16,7 +16,6 @@ cmp.setup({
         })
     },
     snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
             vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
             -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
