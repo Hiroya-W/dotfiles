3213b29ed7078eab3d3cae3e603944efdaf2da11
Hiroya-W <hyuyukun@gmail.com>
2022-05-05 23:27:54 +0900

    :recycle: Use "" as nmap

--- config/nvim/lua/rc/plugins/vim-asterisk.lua
+++ config/nvim/lua/rc/plugins/vim-asterisk.lua
@@ -2,17 +2,11 @@
 -- *: 順方向検索, #: 逆方向検索
 -- 単語区切りでマッチする
 -- <fuga>は hogefugahoge のfugaにマッチしない
-vim.keymap.set("n", "*", "<Plug>(asterisk-z*)")
-vim.keymap.set("v", "*", "<Plug>(asterisk-z*)")
-
-vim.keymap.set("n", "#", "<Plug>(asterisk-z#)")
-vim.keymap.set("v", "#", "<Plug>(asterisk-z#)")
+vim.keymap.set("", "*", "<Plug>(asterisk-z*)")
+vim.keymap.set("", "#", "<Plug>(asterisk-z#)")
 
 -- 単語区切りでマッチさせない
-vim.keymap.set("n", "g*", "<Plug>(asterisk-gz*)")
-vim.keymap.set("v", "g*", "<Plug>(asterisk-gz*)")
-
-vim.keymap.set("n", "g#", "<Plug>(asterisk-gz#)")
-vim.keymap.set("v", "g#", "<Plug>(asterisk-gz#)")
+vim.keymap.set("", "g*", "<Plug>(asterisk-gz*)")
+vim.keymap.set("", "g#", "<Plug>(asterisk-gz#)")
 
 vim.g["asterisk#keeppos"] = 1
