4aa777e3a9deafdefcbec967bf8e46f6ea43559c
Hiroya-W <hyuyukun@gmail.com>
2022-05-07 19:42:22 +0900

    :+1: Add nmap for committia

--- config/nvim/lua/rc/plugins/committia.lua
+++ config/nvim/lua/rc/plugins/committia.lua
@@ -4,6 +4,8 @@ vim.cmd([[
         " Scroll the diff window from insert mode
         " Map <C-n> and <C-p>
         imap <buffer><C-n> <Plug>(committia-scroll-diff-down-half)
+        nmap <buffer><C-n> <Plug>(committia-scroll-diff-down-half)
         imap <buffer><C-p> <Plug>(committia-scroll-diff-up-half)
+        nmap <buffer><C-p> <Plug>(committia-scroll-diff-up-half)
     endfunction
 ]])
