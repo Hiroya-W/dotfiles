d529b803d7d4bcdbfa5d649eda74892b96e9b3fc
Hiroya-W <hyuyukun@gmail.com>
2022-05-09 00:53:52 +0900

    :wastebasket: Ignore custom filter in nvim-tree

--- config/nvim/lua/rc/plugins/nvim-tree.lua
+++ config/nvim/lua/rc/plugins/nvim-tree.lua
@@ -28,7 +28,7 @@ require 'nvim-tree'.setup {
     },
     filters = {
         dotfiles = false,
-        custom = { "^\\.git" },
+        -- custom = { "^\\.git" },
         exclude = {},
     },
 }
