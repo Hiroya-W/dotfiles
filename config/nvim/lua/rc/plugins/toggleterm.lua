ff54960ee32b772bb84020092cbb49c9482fd52c
Hiroya-W <hyuyukun@gmail.com>
2022-05-09 00:39:57 +0900

    :+1: Set keymap in terminal mode

--- config/nvim/lua/rc/plugins/toggleterm.lua
+++ config/nvim/lua/rc/plugins/toggleterm.lua
@@ -1,4 +1,4 @@
-require("toggleterm").setup{
+require("toggleterm").setup {
     size = 20,
     open_mapping = [[<C-t>]],
     shading_factor = 2,
@@ -7,6 +7,15 @@ require("toggleterm").setup{
         border = "curved",
     }
 }
+
+local opts = { noremap = true, silent = true }
+-- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+-- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+vim.keymap.set('t', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+vim.keymap.set('t', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+vim.keymap.set('t', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+vim.keymap.set('t', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+
 
 -- Custom Terminals
 -- Exec: `:lua _FUNCTION_NAME()`
@@ -18,7 +27,14 @@ function _HTOP_TOGGLE()
     htop:toggle()
 end
 
-local python = Terminal:new({ cmd = "python", hidden = true })
+local python = Terminal:new({
+    cmd = "python",
+    direction = "horizontal",
+    hidden = true,
+    on_open = function(term)
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", opts)
+    end
+})
 
 function _PYTHON_TOGGLE()
     python:toggle()
