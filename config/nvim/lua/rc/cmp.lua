37ffd786e992c0249fac2a779268258018fbf9bf
Hiroya-W <hyuyukun@gmail.com>
2022-05-11 00:23:37 +0900

    :+1: Use mapping provided nvim-cmp wiki

--- config/nvim/lua/rc/cmp.lua
+++ config/nvim/lua/rc/cmp.lua
@@ -16,15 +16,10 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
--- https://github.com/L3MON4D3/LuaSnip
--- supertab like completion to work well.
-local check_backspace = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
@@ -71,12 +66,10 @@ cmp.setup({
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expandable() then
-                luasnip.expand()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif check_backspace() then
-                fallback()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
@@ -102,6 +95,7 @@ cmp.setup({
         { name = 'nvim_lua' }, -- This source will complete neovim's Lua runtime API such `vim.lsp.*`.
         { name = "copilot" },
         { name = 'luasnip' }, -- For luasnip users.
+        { name = 'vsnip' }, -- For vsnip
         { name = 'buffer' },
     }),
 })
