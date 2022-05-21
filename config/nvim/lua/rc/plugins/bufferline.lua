8bd85869c5747bc20cfd64867f3455f8d7f9ed74
Hiroya-W <hyuyukun@gmail.com>
2022-05-21 22:53:14 +0900

    :+1: Set mappings to control bufferline

--- config/nvim/lua/rc/plugins/bufferline.lua
+++ config/nvim/lua/rc/plugins/bufferline.lua
@@ -1,2 +1,6 @@
 require("bufferline").setup {
 }
+
+local opts = { noremap = true }
+vim.keymap.set("n", "<leader>bc", ":BufferLinePickClose<CR>", opts)
+vim.keymap.set("n", "<leader>bp", ":BufferLinePick<CR>", opts)
