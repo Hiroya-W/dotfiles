f092e0f44f64fabca61cee5ad7d23d2b9951eb99
Hiroya-W <hyuyukun@gmail.com>
2022-05-31 18:13:13 +0900

    :art: Add tokyonight color scheme

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,6 +8,20 @@ local function default_prog()
     end
 end
 
+local function tokyonight_color()
+    return {
+        foreground = "#c0caf5",
+        background = "#1a1b26",
+        cursor_bg = "#c0caf5",
+        cursor_border = "#c0caf5",
+        cursor_fg = "#1a1b26",
+        selection_bg = "#33467C",
+        selection_fg = "#c0caf5",
+        ansi = {"#15161E", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#a9b1d6"},
+        brights = {"#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff", "#c0caf5"},
+    }
+end
+
 return {
     default_prog = default_prog(),
     font = wezterm.font("HackGenNerd Console"),
@@ -15,4 +29,5 @@ return {
     font_size = 11.0;
     hide_tab_bar_if_only_one_tab = true,
     adjust_window_size_when_changing_font_size = false,
+    colors = tokyonight_color()
 }
