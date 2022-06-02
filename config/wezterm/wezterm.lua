8665484ca5769bef858ea6fe60f34433ba9e9b76
Hiroya-W <hyuyukun@gmail.com>
2022-06-02 19:50:48 +0900

    :mute: don't check update
    instead of pacman package manager

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -17,5 +17,6 @@ return {
     font_size = 11.0;
     hide_tab_bar_if_only_one_tab = true,
     adjust_window_size_when_changing_font_size = false,
-    show_update_window = false
+    show_update_window = false,
+    check_for_updates = false,
 }
