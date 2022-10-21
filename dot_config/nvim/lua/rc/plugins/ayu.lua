040436f4bdaa63a216a4dd9edf33a6d399d9bc7e
Hiroya-W <hyuyukun@gmail.com>
2022-10-22 01:47:50 +0900

    :recycle: Move configs to ayu.lua

---
+++ dot_config/nvim/lua/rc/plugins/ayu.lua
@@ -0,0 +1,37 @@
+local colors = require('ayu.colors')
+require("ayu").setup({
+    overrides = {
+        Comment = { fg = colors.comment },
+        LspSignatureActiveParameter = { italic = false }
+    }
+})
+vim.cmd [[colorscheme ayu-light]]
+vim.g.terminal_color_0 = "#000000"
+vim.g.terminal_color_8 = "#323232"
+
+-- red
+vim.g.terminal_color_1 = "#ff3333"
+vim.g.terminal_color_9 = "#ff6565"
+
+-- green
+vim.g.terminal_color_2 = "#86b300"
+vim.g.terminal_color_10 = "#b8e532"
+
+-- yellow
+vim.g.terminal_color_3 = "#f29718"
+vim.g.terminal_color_11 = "#ffc94a"
+
+-- blue
+vim.g.terminal_color_4 = "#41a6d9"
+vim.g.terminal_color_12 = "#73d8ff"
+
+-- magenta
+vim.g.terminal_color_5 = "#f07178"
+vim.g.terminal_color_13 = "#ffa3aa"
+
+-- cyan
+vim.g.terminal_color_6 = "#4dbf99"
+vim.g.terminal_color_14 = "#7ff1cb"
+
+vim.g.terminal_color_7 = "#ffffff"
+vim.g.terminal_color_15 = "#ffffff"
