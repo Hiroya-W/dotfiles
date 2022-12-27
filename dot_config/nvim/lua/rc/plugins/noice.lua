7af7a21621543d96f3456f44bb2ad34aba994d44
Hiroya-W <hyuyukun@gmail.com>
2022-12-27 17:06:02 +0900

    :+1: Use suggested setup for noice

--- dot_config/nvim/lua/rc/plugins/noice.lua
+++ dot_config/nvim/lua/rc/plugins/noice.lua
@@ -1,27 +1,46 @@
-local noice = require("noice")
+-- local noice = require("noice")
+--
+-- require("noice").setup({
+--     cmdline = {
+--         view = "cmdline"
+--     },
+--     views = {
+--         popupmenu = {
+--             relative = "editor",
+--             position = {
+--                 row = "100%",
+--                 col = 0,
+--             },
+--             size = {
+--                 width = 60,
+--                 height = 10,
+--             },
+--             border = {
+--                 style = "rounded",
+--                 padding = { 0, 1 },
+--             },
+--             win_options = {
+--                 winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+--             },
+--         },
+--     },
+-- })
 
 require("noice").setup({
-    cmdline = {
-        view = "cmdline"
-    },
-    views = {
-        popupmenu = {
-            relative = "editor",
-            position = {
-                row = "100%",
-                col = 0,
-            },
-            size = {
-                width = 60,
-                height = 10,
-            },
-            border = {
-                style = "rounded",
-                padding = { 0, 1 },
-            },
-            win_options = {
-                winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-            },
+    lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
         },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
     },
 })
