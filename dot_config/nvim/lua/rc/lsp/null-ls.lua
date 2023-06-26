8ddd79578831182c943c18db8f52c67d16665198
Hiroya-W <hyuyukun@gmail.com>
2023-06-26 15:43:42 +0900

    :sparkles: Automatic install for non LSPs

--- dot_config/nvim/lua/rc/lsp/null-ls.lua
+++ dot_config/nvim/lua/rc/lsp/null-ls.lua
@@ -3,6 +3,19 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
+require("mason-null-ls").setup({
+    ensure_installed = {
+        "black",
+        "isort",
+        "stylua",
+        "flake8",
+        "mypy",
+        "golangci_lint"
+    },
+    automatic_installation = false,
+    handlers = {},
+})
+
 require("null-ls").setup({
     sources = {
         -- formatting
