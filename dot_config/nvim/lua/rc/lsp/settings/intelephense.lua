66d140874b7128b48f399c696f193dd71ca95735
Hiroya-W <hyuyukun@gmail.com>
2024-04-27 14:40:17 +0900

    :sparkles: ライセンスを読み込む

--- dot_config/nvim/lua/rc/lsp/settings/intelephense.lua
+++ dot_config/nvim/lua/rc/lsp/settings/intelephense.lua
@@ -1,7 +1,20 @@
+-- https://alextheobold.com/posts/intelephense_in_neovim/
+local get_intelephense_license = function()
+    local f = assert(io.open(os.getenv("HOME") .. "/intelephense/license.txt", "rb"))
+
+    local content = f:read("*a")
+
+    f:close()
+
+    return string.gsub(content, "%s+", "")
+end
+
 return {
+    init_options = {
+        licenceKey = get_intelephense_license()
+    },
     settings = {
         intelephense = {
-
         }
     }
 }
