e977235a258362f88aedded3b1dcfa4309e375a1
Hiroya-W <hyuyukun@gmail.com>
2024-05-02 15:45:37 +0900

    :wrench: Install skkeleton_indicator.nvim
    ndicator# ==================== Emojis ====================

--- dot_config/nvim/lua/rc/plugins.lua
+++ dot_config/nvim/lua/rc/plugins.lua
@@ -15,14 +15,22 @@ require("lazy").setup({
     {
         "vim-denops/denops.vim",
         dependencies = {
-            "vim-skk/skkeleton",
-            config = function()
-                vim.fn["skkeleton#config"]({
-                    globalDictionaries = {
-                        "~/.local/share/skk/SKK-JISYO.L"
-                    }
-                })
-            end
+            {
+                "vim-skk/skkeleton",
+                config = function()
+                    vim.fn["skkeleton#config"]({
+                        globalDictionaries = {
+                            "~/.local/share/skk/SKK-JISYO.L"
+                        }
+                    })
+                end
+            },
+            {
+                "delphinus/skkeleton_indicator.nvim",
+                config = function()
+                    require("skkeleton_indicator").setup()
+                end
+            }
         }
     },
     {
