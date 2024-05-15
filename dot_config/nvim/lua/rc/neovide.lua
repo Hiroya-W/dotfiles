0806204942fbae72b17570616b44ef33257f494a
Hiroya-W <hyuyukun@gmail.com>
2024-05-15 17:00:39 +0900

    :+1: Allow clipboard copy paste on Neovide

--- dot_config/nvim/lua/rc/neovide.lua
+++ dot_config/nvim/lua/rc/neovide.lua
@@ -15,4 +15,12 @@ if vim.g.neovide == true then
         { silent = true }
     )
     vim.keymap.set("n", "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>", { silent = true })
+
+    -- Allow clipboard copy paste in neovim
+    -- https://github.com/neovide/neovide/issues/1263#issuecomment-1100895622
+    vim.g.neovide_input_use_logo = 1
+    vim.keymap.set('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+    vim.keymap.set('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+    vim.keymap.set('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+    vim.keymap.set('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
 end
