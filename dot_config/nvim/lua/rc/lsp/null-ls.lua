9c8bad418666ced402610a2dff6cdaf8b9d25db0
Hiroya-W <hyuyukun@gmail.com>
2024-11-09 17:16:31 +0900

    :sparkles: Non-LSP フロントエンドで必要そうなものをインストール

--- dot_config/nvim/lua/rc/lsp/null-ls.lua
+++ dot_config/nvim/lua/rc/lsp/null-ls.lua
@@ -1,7 +1,7 @@
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
--- local code_actions = null_ls.builtins.code_actions
+local code_actions = null_ls.builtins.code_actions
 
 require("mason-null-ls").setup({
     ensure_installed = {
@@ -12,7 +12,11 @@ require("mason-null-ls").setup({
         "mypy",
         "golangci_lint",
         "phpstan", -- intelephense が phpstan を出してくれるのでバイナリだけ入れておく
-        "easy-coding-standard"
+        "easy-coding-standard",
+        "eslint_d",
+        "prettierd",
+        "yamllint",
+        "markdownlint",
     },
     automatic_installation = false,
     handlers = {},
@@ -28,14 +32,21 @@ require("null-ls").setup({
         formatting.stylua,
         -- PHP
         require("none-ls-ecs.formatting"),
+        -- JS/TS
+        formatting.prettierd,
+        require("none-ls.diagnostics.eslint_d"),
         -- diagnostics
         -- Python
         diagnostics.flake8,
         diagnostics.mypy,
         -- Golang
         diagnostics.golangci_lint,
+        diagnostics.yamllint,
+        diagnostics.markdownlint,
+
         -- code_actions
         -- code_actions.refactoring,
+        require("none-ls.code_actions.eslint_d"),
     },
     on_attach = require('lsp-format').on_attach
 })
