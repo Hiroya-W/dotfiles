c3b70f41826d3076d79c5402ade80f5398fc1518
Hiroya-W <hyuyukun@gmail.com>
2024-11-09 17:05:37 +0900

    :wrench: none-ls用の追加のsourceをインストール

--- dot_config/nvim/lua/rc/plugins.lua
+++ dot_config/nvim/lua/rc/plugins.lua
@@ -236,7 +236,9 @@ require("lazy").setup({
             "nvim-lua/plenary.nvim",
             "williamboman/mason.nvim",
             "nvimtools/none-ls.nvim",        -- Use non LSP sources
+            "nvimtools/none-ls-extras.nvim", -- Extra sources
             "lukas-reineke/lsp-format.nvim", -- format on save
+            "gbprod/none-ls-ecs.nvim", -- PHP formatter source
         },
         config = function()
             require("rc/lsp/null-ls")
