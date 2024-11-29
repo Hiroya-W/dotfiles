848766a3252550f321ab6efe6bc95c4aee9b9b73
Hiroya-W <hyuyukun@gmail.com>
2024-11-30 01:02:39 +0900

    :wrench: Install gitlab.nvim

--- dot_config/nvim/lua/rc/plugins.lua
+++ dot_config/nvim/lua/rc/plugins.lua
@@ -356,6 +356,22 @@ require("lazy").setup({
             require("rc/plugins/blame")
         end
     },
+    {
+        "harrisoncramer/gitlab.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "stevearc/dressing.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        build = function()
+            require("gitlab.server").build(true)
+        end,
+        config = function()
+            require("rc/plugins/gitlab")
+        end,
+    },
     --------------------------------------------------------------
     -- UI
     {
