3f46f61af8e06765b6e037ca75e9f9f2b48f1e1b
Hiroya-W <hyuyukun@gmail.com>
2024-11-30 01:03:46 +0900

    :wrench: Install smart-open.nvim

--- dot_config/nvim/lua/rc/plugins.lua
+++ dot_config/nvim/lua/rc/plugins.lua
@@ -280,6 +280,20 @@ require("lazy").setup({
                     require("telescope").load_extension("neoclip")
                 end,
             },
+            {
+                "danielfalk/smart-open.nvim",
+                branch = "0.2.x",
+                config = function()
+                    require("telescope").load_extension("smart_open")
+                end,
+                dependencies = {
+                    "kkharji/sqlite.lua",
+                    -- Only required if using match_algorithm fzf
+                    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+                    -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+                    { "nvim-telescope/telescope-fzy-native.nvim" },
+                },
+            },
         },
         config = function()
             require("rc/plugins/telescope")
